cc2c58a4ef2dcc2b4c75f857893805ac0a6001f3
Keisuke-Igarashi <igarashi4948@gmial.com>
2022-06-15 22:31:09 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+export class main {
+    constructor(el) {
+        this.el = el;
+        this.name = 'yama';
+        this.age = 32;
+    }
+    
+    _btn_onclick() {
+        window.open('test2.html');
+    }
+}
+
+
+// var main_ob = new main('Hello World');
---
+++ sub.js
@@ -0,0 +1,4 @@
+import {main} from './main.js';
+
+var main_sub = new main(); 
+var name_2 = main_sub.name;
